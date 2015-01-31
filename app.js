28a3ed1447549e11c76c97fde39161b419302889
Shayak Dutta <shayak007@gmail.com>
2015-01-31 17:34:56 -0500

    added node version

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
-app.listen(5000);
+app.listen(process.env.PORT || 5000);
 
 console.log('listening on 1123...');
